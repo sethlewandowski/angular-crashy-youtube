2e67a8b5eacefb03f7709fab100dcf32e15eabb3
Seth Lewandowski <sethlew@protonmail.com>
2021-03-30 18:07:09 -0700

    set up init home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // injection point
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angularCrashy';
+  title = 'angularCrashy'; // only var
+  date = new Date();
+  name: string = 'seth';
 }
